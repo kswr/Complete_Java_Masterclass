fbd8f57a7875fa6901477916593d605fd303648d
kswr <ksawery.oleszynski@gmail.com>
2017-12-08 22:52:17 +0100

    lecture 16, variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,2 +1,19 @@
 public class Hello {
+    public static void main(String[] args){
+
+        System.out.println("Hello, World!");
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+
+        System.out.println(myFirstNumber );
+        System.out.println("myFirstNumber");
+
+        int mySecondNumber = 12;
+        int myThirdNubmer = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNubmer;
+
+        System.out.println(myTotal);
+
+    }
 }
