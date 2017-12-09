baece97f8f8b200e1ce30ac879c4c52cd4df2289
kswr <ksawery.oleszynski@gmail.com>
2017-12-09 11:14:50 +0100

    finished lecture 20

--- CharAndBoolean/src/com/kswr/Main.java
+++ CharAndBoolean/src/com/kswr/Main.java
@@ -3,6 +3,22 @@ package com.kswr;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ipsum");
+
+        // char has width of 16 (2 bytes)
+        char myCharValue = 'a'; // can't be more than one character
+        char mySecondChar = '\u00A9';
+        System.out.println("myCharValue = " + myCharValue);
+        System.out.println("Unicode character is " + mySecondChar);
+
+        // Boolean width is machie dependent
+        boolean myBoolean = true;
+        boolean mySecondBoolean = false;
+
+        // CHALLENGE
+
+        // display registered symbol
+
+        char registered = '\u00AE';
+        System.out.println("Registered symbol is " + registered);
     }
 }
