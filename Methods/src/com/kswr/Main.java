5df7b37a3cb3de32bea082f5c56b8f37e009e03f
kswr <ksawery.oleszynski@gmail.com>
2017-12-10 10:33:09 +0100

    finished lecture 28

--- Methods/src/com/kswr/Main.java
+++ Methods/src/com/kswr/Main.java
@@ -3,8 +3,19 @@ package com.kswr;
 public class Main {
 
     public static void main(String[] args) {
-        calculateScore(true, 800, 5, 10);
-        calculateScore(true, 10000, 8, 200);
+        int higscore = calculateScore(true, 800, 5, 10);
+        System.out.println("Your final score was " + higscore);
+
+        int highscore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highscore);
+
+        int position = calculateHighscorePosition(900);
+        System.out.println("His position was " + position);
+
+        displayHighscorePosition("Joe", 1500);
+        displayHighscorePosition("Ben", 900);
+        displayHighscorePosition("Hoe", 400);
+        displayHighscorePosition("John", 50);
     }
 
     public static int calculateScore(boolean game0ver, int score, int levelCompleted, int bonus) {
@@ -15,5 +26,21 @@ public class Main {
         } else {
             return -1;
         }
+    }
+
+    public static void displayHighscorePosition(String name, int points) {
+        System.out.println(name + " managed to get into position " + calculateHighscorePosition(points));
+
+    }
+
+    public static int calculateHighscorePosition(int points) {
+        if(points > 1000) {
+            return 1;
+        } else if ((points <= 1000) && (points > 500)) {
+            return 2;
+        } else if ((points <= 500) && (points > 100)) {
+            return 3;
+        }
+        return 4;
     }
 }
