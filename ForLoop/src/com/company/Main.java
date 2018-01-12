ce6fa69cfb42d22c197f171426783e97a071a5e3
kswr <ksawery.oleszynski@gmail.com>
2018-01-12 17:55:04 +0100

    lecture 32 in the middle of doing

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -3,22 +3,49 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("\ncalculateInterest method sample output:\n");
         System.out.println("10.000 at 2% interest = " + calculateInterest(10000.0, 2.0));
 
         // I don't write the dumb part of code for printing out above string with various values, instead I will use loop
 
         // example loop
 
+        System.out.println("\nSample loop:\n");
+
         for (int i = 0; i < 5; i++){
             System.out.println("Loop " + i + " hello!");
         }
 
-        // CHALLENGE
+        // CHALLENGE part 1
         // using for loop calculate investment interest for 10,000 dollars with interest rates from ranges 2-8
 
+        System.out.println("\nChallange part 1. result:\n");
+
         for (double i = 2; i < 9; i++) {
-            System.out.println(calculateInterest(10000.0, i));
+//            System.out.println(calculateInterest(10000.0, i));
+            System.out.println("10.000 with " + i + " interest is " + String.format("%.2f", calculateInterest(10000.0,i)));
         }
+
+        // Challenge part 2
+        // modify above loop to go from 9 to 2
+
+        System.out.println("\nChallange part 2. result:\n");
+
+        for (double i = 9; i > 1; i--) {
+            System.out.println("10.000 with " + i + " interest is " + String.format("%.2f", calculateInterest(10000.0,i)));
+        }
+
+        // Challenge part 3
+        // count primes in given sequence of numbers, break if number hits 3
+
+        System.out.println("\nChallenge part 3. result:\n");
+
+        System.out.println("Found primes: " + countPrimes(4,10));
+
+        System.out.println("\nChallenge part 3. second solution");
+
+//        for (int i = 10; int y = 50; i++)
     }
 
     // method for printing fictional coefficient of investment interest
@@ -26,4 +53,33 @@ public class Main {
     public static double calculateInterest(double amount, double interestRate) {
          return(amount * (interestRate/100));
     }
+
+    public static boolean isPrime(int n) {
+
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int countPrimes(int starting, int ending) {
+        int count = 0;
+        for (int i = starting; i <= ending; i++) {
+           if (isPrime(i)) {
+               count++;
+           }
+           if (count == 3) {
+               break;
+           }
+        }
+        return count;
+    }
+
 }
---
+++ Notes.md
@@ -0,0 +1,13 @@
+# Complete Java Masterclass - Notes
+
+## Questions
+
+- Why byte is 2^7 not 2^8
+- Binary vs hexadecimal <<< learn
+- Detailed difference between statement and expression
+- Flashcards on
+  - [coercion, conversion and casting](https://en.wikipedia.org/wiki/Type_conversion)
+  - variable
+  - constant
+  - literal
+  - repository
