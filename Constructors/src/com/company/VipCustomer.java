f9c82a02e7b73983afb73eabba2c4434f784d193
kswr <ksawery.oleszynski@gmail.com>
2018-04-12 13:33:14 +0200

    Constructors finished (second time)

--- Constructors/src/com/company/VipCustomer.java
+++ Constructors/src/com/company/VipCustomer.java
@@ -1,43 +1,33 @@
 package com.company;
 
 public class VipCustomer {
+    private String name;
+    private double creditLimit;
+    private String emailAddress;
 
-    // Create a new class VipCustomer
-    // it should have 3 fields name, credit limit, and email address.
-    // create 3 constructors
-    // 1st constructor empty should call the constructor with 3 parameters with default values
-    // 2nd constructor should pass on the 2 values it receives and add a default value for the 3rd
-    // 3rd constructor should save all fields.
-    // create getters only for this using code generation of intellij as setters wont be needed
-    // test and confirm it works.
+    VipCustomer(String name, double creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
 
-   private String name;
-   private String creditLimit;
-   private String email;
+    VipCustomer(String name, double creditLimit) {
+        this(name, creditLimit, "default.email@gmail.com");
+    }
 
-   public VipCustomer(String name, String creditLimit, String email) {
-       this.name = name;
-       this.creditLimit = creditLimit;
-       this.email = email;
-   }
-
-   public VipCustomer(String name, String creditLimit) {
-       this(name, creditLimit, "default.email@email.com");
-   }
-
-   public VipCustomer() {
-       this("Default Name", "Default credit limit", "Default email");
-   }
+    VipCustomer() {
+        this("Default Name", 100d, "default.email@gmail.com");
+    }
 
     public String getName() {
         return name;
     }
 
-    public String getCreditLimit() {
+    public double getCreditLimit() {
         return creditLimit;
     }
 
-    public String getEmail() {
-        return email;
+    public String getEmailAddress() {
+        return emailAddress;
     }
 }
