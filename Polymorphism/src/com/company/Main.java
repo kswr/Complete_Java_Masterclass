b5fe2fa11de3a997839023892d79c1a6320329f2
kswr <ksawery.oleszynski@gmail.com>
2018-04-15 18:15:32 +0200

    polymorphism finished

--- Polymorphism/src/com/company/Main.java
+++ Polymorphism/src/com/company/Main.java
@@ -23,7 +23,7 @@ class Jaws extends Movie {
 
     @Override
     public String plot() {
-        return "A shark eats a lot of people"
+        return "A shark eats a lot of people";
     }
 }
 
@@ -49,9 +49,50 @@ class MazeRunner extends Movie {
     }
 }
 
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Some geek shiet";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable() {
+        super("Forgetable");
+    }
+
+    // No plot this time
+}
+
 public class Main {
 
     public static void main(String[] args) {
+        for(int i=1; i<11; i++){
+            Movie movie = randomMovie();
+            System.out.printf("Movie # %d: %s%nPlot: %s%n",i, movie.getName(), movie.plot());
+        }
 
+    }
+
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) +1;
+        System.out.println("Random number generated was " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependanceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+        }
+        return null;
     }
 }
