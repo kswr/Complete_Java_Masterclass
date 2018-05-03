bf007598b33d4a445f27b5e435fe84f8cc24144f
kswr <ksawery.oleszynski@gmail.com>
2018-05-04 00:51:51 +0200

    threads

--- Threads/src/com/company/AnotherThread.java
+++ Threads/src/com/company/AnotherThread.java
@@ -4,7 +4,16 @@ import static com.company.ThreadColor.ANSI_BLUE;
 
 public class AnotherThread extends Thread {
     @Override
-    public void run() {
-        System.out.println(ANSI_BLUE + "Hello from another thread");
+    public void run(){
+        System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println(ANSI_BLUE + "Another thread woke me up");
+            return;
+        }
+
+        System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake");
     }
 }
--- Threads/src/com/company/Main.java
+++ Threads/src/com/company/Main.java
@@ -1,14 +1,13 @@
 package com.company;
 
-import static com.company.ThreadColor.ANSI_BLACK;
-import static com.company.ThreadColor.ANSI_CYAN;
-import static com.company.ThreadColor.ANSI_GREEN;
+import static com.company.ThreadColor.*;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(ANSI_BLACK + "Hello from the main thread");
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("=== Another Thread ===");
         anotherThread.start();
 
         new Thread() {
@@ -16,6 +15,24 @@ public class Main {
                 System.out.println(ANSI_GREEN + "Hello from anonymous class thread");
             }
         }.start();
+
+//        Thread myRunnableThread = new Thread(new myRunnable());
+
+        Thread myRunnableThread = new Thread(new myRunnable(){
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + "Hello from anonymous class implementation of run()");
+                try {
+                    anotherThread.join(2000);
+                    System.out.println(ANSI_RED + "Another thread terminated or timed out, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I coldn't wait after all. I was interrupted");
+                }
+            }
+        });
+        myRunnableThread.start();
+        anotherThread.interrupt();
+//        myRunnableThread.interrupt();
 
         System.out.println(ANSI_BLACK + "Hello again from the main thread");
     }
