9ee0189019d0060321b825912692f752c5081425
kswr <ksawery.oleszynski@gmail.com>
2018-05-03 22:27:36 +0200

    threads

--- Threads/src/com/company/Main.java
+++ Threads/src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import static com.company.ThreadColor.ANSI_BLACK;
+import static com.company.ThreadColor.ANSI_CYAN;
+import static com.company.ThreadColor.ANSI_GREEN;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(ANSI_BLACK + "Hello from the main thread");
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
 
+        new Thread() {
+            public void run() {
+                System.out.println(ANSI_GREEN + "Hello from anonymous class thread");
+            }
+        }.start();
+
+        System.out.println(ANSI_BLACK + "Hello again from the main thread");
     }
 }
