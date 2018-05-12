9dede404716d10eda5030ee805b912aac7a1455b
kswr <ksawery.oleszynski@gmail.com>
2018-05-12 02:01:19 +0200

    sample coding exercise

---
+++ SampleCodingExercise/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        checkNumber(1);
+        checkNumber(0);
+        checkNumber(-1);
+    }
+
+    public static void checkNumber(int number) {
+
+        if (number == 0) {
+            System.out.println("Number " + number + " is equal to 0");
+        } else if (number > 0) {
+            System.out.println("Number " + number + " is greater than 0");
+        } else {
+            System.out.println("Number " + number + " is lesser than 0");
+        }
+    }
+}
